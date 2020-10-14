97ddc7fd422e2b2ca433cb1bf43a2dba2a912e67
yuki-nakano <nyuki0614@gmail.com>
2020-10-14 18:03:33 +0900

    PlayerとEnemyのパラメーター描画処理

--- 02_1014/Practice02/practice02_04/practice02_04.cpp
+++ 02_1014/Practice02/practice02_04/practice02_04.cpp
@@ -5,10 +5,29 @@
 
 int main()
 {
-    
+    Player player;
+    player.SetMoveSpeed(5.0f);
 
+    Enemy enemy;
+    enemy.SetHp(100);
 
+	printf("-------------\n");
+	printf("PRACTICE02_04\n");
+	printf("-------------\n");
 
+	printf("Player Status\n");
+	printf("hp = %d\n", player.GetHp());
+	printf("PosX = %0.2f\n", player.GetPosX());
+	printf("PosY = %0.2f\n", player.GetPosY());
+	printf("MoveSpeed = %0.2f\n", player.GetMoveSpeed());
+	printf("\n");
+
+	printf("Player Status\n");
+	printf("hp = %d\n", enemy.GetHp());
+	printf("PosX = %0.2f\n", enemy.GetPosX());
+	printf("PosY = %0.2f\n", enemy.GetPosY());
+	printf("MoveSpeed = %0.2f\n", enemy.GetMoveSpeed());
+	printf("\n");
 
 
     system("pause");
