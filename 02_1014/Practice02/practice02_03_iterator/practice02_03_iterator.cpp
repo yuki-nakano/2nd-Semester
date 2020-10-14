c5b2f83f592d533dc0ba45dbc0a8d039c1b82184
yuki-nakano <nyuki0614@gmail.com>
2020-10-14 16:39:26 +0900

    iteratorの使い方に関する実習用プロジェクトの作成と、実作業

---
+++ 02_1014/Practice02/practice02_03_iterator/practice02_03_iterator.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdlib.h>
+#include <stdlib.h>
+#include <vector>	//vectorを扱うためのヘッダー	
+#include <iostream>
+#include <time.h>
+
+int main()
+{
+    srand((unsigned)time(NULL));
+ 
+    std::vector<int> v1(8);         //要素８、すべてを0で初期化
+
+    //iteratorを使ったfor文の回し方
+    //型：std::vector<int>;;iterator
+    std::vector<int>::iterator itr;
+    for (itr = v1.begin(); itr != v1.end(); itr++)
+    {
+        *itr = rand();
+        printf("%d\n", *itr);
+    }
+    printf("\n");
+
+    system("pause");
+    return 0;
+}
+
