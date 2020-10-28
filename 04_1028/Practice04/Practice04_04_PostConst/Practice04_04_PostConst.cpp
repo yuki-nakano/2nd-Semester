279554b76bdc3cbdad4baa2e84a184726c41edc6
yuki-nakano <nyuki0614@gmail.com>
2020-10-28 17:57:11 +0900

    第４回実習その４
    後置コンスト(＝コンストメンバ関数)
    　書き方、使い方
    　どのような効果があるか
    　どういう関数に使えばいいか

---
+++ 04_1028/Practice04/Practice04_04_PostConst/Practice04_04_PostConst.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+//後置const
+class Test
+{
+public:
+	void SetValue(int value);
+	int GetValue() const;
+private:
+	int Value;
+};
+
+void Test::SetValue(int value)
+{
+	Value = value;
+}
+//コンストメンバ関数の定義(実装)部分にも後置constをつける
+int Test::GetValue() const
+{
+	//コンストメンバ関数の中では、メンバ変数に対する値の代入を行うことができない
+	//コンストメンバ関数を呼び出す際、内部のメンバ変数への代入操作を行わないことが保証されている
+	//Value = 100;
+	
+	//コンストメンバ関数以外の関数を呼び出すこともできない
+	//SetValue(100);
+	
+	return Value;
+}
+
+
+int main()
+{
+	Test test;
+	test.SetValue(1000);
+	int ans = test.GetValue();
+
+	//データを取得したいだけの時
+	//ポインタ変数の先に変更を加えないことを保証するため
+	//constポインタ変数を使うことが一般的
+	const Test* pTest = &test;
+	//pTest->SetValue(10);	//コンストメンバ関数でないため、使用できない(変更される恐れあり)
+	pTest->GetValue();		//コンストメンバ関数なので、使用できる
+}
